8ba800cb1bbce551855419003adddfd42bc82023
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-08-04 19:39:22 +0800

    up

---
+++ git-test/test.js
@@ -0,0 +1,17 @@
+function test1() {
+  console.log("test1");
+}
+
+function test2() {
+  console.log("test1");
+}
+
+function test3() {
+  console.log("test1");
+}
+
+function main() {
+  test1();
+  test2();
+  test3();
+}
